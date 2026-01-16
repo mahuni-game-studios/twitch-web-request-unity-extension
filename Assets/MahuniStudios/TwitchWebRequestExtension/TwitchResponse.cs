fd757735c401e2ce21a76967001c7c1181053a71
LaMahuni <la@mahuni.com>
2026-01-16 18:06:24 +0100

    Added more Twitch reponse codes

--- Assets/MahuniStudios/TwitchWebRequestExtension/TwitchResponse.cs
+++ Assets/MahuniStudios/TwitchWebRequestExtension/TwitchResponse.cs
@@ -12,11 +12,14 @@ namespace Mahuni.Twitch.Extension
     public enum TwitchResponseCode
     {
         OK = 200,
+        ACCEPTED = 202,
         NO_CONTENT = 204,
         BAD_REQUEST = 400,
         UNAUTHORIZED = 401,
         FORBIDDEN = 403,
         NOT_FOUND = 404,
+        CONFLICT = 409,
+        TOO_MANY_REQUESTS = 429,
         INTERNAL_SERVER_ERROR = 500,
     }
     
